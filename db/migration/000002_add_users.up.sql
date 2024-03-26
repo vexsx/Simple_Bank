f5137608ddc35fe2fb4f41f3a2649b7bd1393dce
vexsx <p.gheiratian1382@yahoo.com>
2024-03-27 00:23:58 +0330

    🙃create user test done!!

--- db/migration/000002_add_users.up.sql
+++ db/migration/000002_add_users.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE "users" (
                          "hashed_password" varchar NOT NULL,
                          "full_name" varchar NOT NULL,
                          "email" varchar UNIQUE NOT NULL,
-                         "pssword_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
+                         "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
                          "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
@@ -11,4 +11,4 @@ ALTER TABLE "accounts" ADD FOREIGN KEY ("owner") REFERENCES "users" ("username")
 
 -- CREATE UNIQUE INDEX ON "accounts" ("owner", "currency");
 
-ALTER TABLE "accounts" ADD CONSTRAINT "owner_currency_key" unique ("owner","currency");
+ALTER TABLE "accounts" ADD CONSTRAINT "owner_currency_key" UNIQUE ("owner","currency");
