6beb3f4cd63d9b4ded02a90925f10c32408f8cb9
vexsx <p.gheiratian1382@yahoo.com>
2024-03-31 18:47:03 +0330

    update add dbdocs update Makefile 🫰✔️

--- Makefile
+++ Makefile
@@ -1,14 +1,17 @@
+
+DB_URL=postgresql://vexsx:New2021!@167.99.138.192:5432/Bank_db?sslmode=disable
+
 migrateup :
-	migrate -path db/migration -database "postgresql://vexsx:New2021!@167.99.138.192:5432/Bank_db?sslmode=disable" -verbose up
+	migrate -path db/migration -database "$(DB_URL)" -verbose up
 
 migrateup1 :
-	migrate -path db/migration -database "postgresql://vexsx:New2021!@167.99.138.192:5432/Bank_db?sslmode=disable" -verbose up 1
+	migrate -path db/migration -database "$(DB_URL)" -verbose up 1
 
 migratedown :
-	migrate -path db/migration -database "postgresql://vexsx:New2021!@167.99.138.192:5432/Bank_db?sslmode=disable" -verbose down
+	migrate -path db/migration -database "$(DB_URL)" -verbose down
 
 migratedown1 :
-	migrate -path db/migration -database "postgresql://vexsx:New2021!@167.99.138.192:5432/Bank_db?sslmode=disable" -verbose down 1
+	migrate -path db/migration -database "$(DB_URL)" -verbose down 1
 
 test:
 	go test -v -cover ./...
@@ -16,4 +19,10 @@ test:
 server :
 	go run main.go
 
-.PHONY: migrateup migrateup  migrateup1 migrateup1 test server
+db_docs:
+	dbdocs build doc/db.dbml
+
+db_schema:
+	dbml2sql --postgres -o doc/schema.sql doc/db.dbml
+
+.PHONY: migrateup migrateup  migrateup1 migrateup1 test server db_docs db_schema
---
+++ doc/schema.sql
@@ -0,0 +1,72 @@
+-- SQL dump generated using DBML (dbml.dbdiagram.io)
+-- Database: PostgreSQL
+-- Generated at: 2024-03-31T15:08:17.949Z
+
+CREATE TABLE "users" (
+  "username" varchar PRIMARY KEY,
+  "hashed_password" varchar NOT NULL,
+  "full_name" varchar NOT NULL,
+  "email" varchar UNIQUE NOT NULL,
+  "pssword_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "accounts" (
+  "id" BIGSERIAL PRIMARY KEY,
+  "owner" varchar NOT NULL,
+  "balance" bigint NOT NULL,
+  "currency" varchar NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "entries" (
+  "id" BIGSERIAL PRIMARY KEY,
+  "account_id" bigint,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "transfers" (
+  "id" BIGSERIAL PRIMARY KEY,
+  "from_account_id" bigint,
+  "to_account_id" bigint,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "sessions" (
+  "id" uuid PRIMARY KEY,
+  "username" varchar NOT NULL,
+  "refresh_token" varchar NOT NULL,
+  "user_agent" varchar NOT NULL,
+  "client_ip" varchar NOT NULL,
+  "is_blocked" BOOLEAN NOT NULL DEFAULT false,
+  "expires_at" timestamptz NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE INDEX ON "accounts" ("owner");
+
+CREATE UNIQUE INDEX ON "accounts" ("owner", "currency");
+
+CREATE INDEX ON "entries" ("account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id");
+
+CREATE INDEX ON "transfers" ("to_account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
+
+COMMENT ON COLUMN "entries"."amount" IS 'can be nagative or posetive';
+
+COMMENT ON COLUMN "transfers"."amount" IS 'must be posetive';
+
+ALTER TABLE "accounts" ADD FOREIGN KEY ("owner") REFERENCES "users" ("username");
+
+ALTER TABLE "sessions" ADD FOREIGN KEY ("username") REFERENCES "users" ("username");
+
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
