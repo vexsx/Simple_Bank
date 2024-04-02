ee5bedb6827c8d070f5f11b4612d876e09f69aa3
vexsx <p.gheiratian1382@yahoo.com>
2024-04-02 13:32:58 +0330

    section 04-07

--- Makefile
+++ Makefile
@@ -27,8 +27,9 @@ db_schema:
 
 proto:
 	protoc --proto_path=proto --go_out=pb --go_opt=paths=source_relative \
-        --go-grpc_out=pb --go-grpc_opt=paths=source_relative \
-        proto/*.proto
+      --go-grpc_out=pb --go-grpc_opt=paths=source_relative \
+      --grpc-gateway_out=pb  --grpc-gateway_opt=paths=source_relative \
+      proto/*.proto
 
 evans:
 	evans --host localhost --port 9090 -r repl
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"github.com/vexsx/Simple-Bank/api"
 	db "github.com/vexsx/Simple-Bank/db/sqlc"
@@ -10,8 +12,10 @@ import (
 	"github.com/vexsx/Simple-Bank/util"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
 	"log"
 	"net"
+	"net/http"
 )
 
 func main() {
@@ -27,6 +31,7 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
+	go runGatewayServer(config, store)
 	runGrpcServer(config, store)
 
 }
@@ -51,6 +56,46 @@ func runGrpcServer(config util.Config, store *db.Store) {
 		log.Fatal("cannot creat grpc server")
 	}
 }
+
+func runGatewayServer(config util.Config, store *db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server", err)
+	}
+
+	jsonOption := runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+		MarshalOptions: protojson.MarshalOptions{
+			UseProtoNames: true,
+		},
+		UnmarshalOptions: protojson.UnmarshalOptions{
+			DiscardUnknown: true,
+		},
+	})
+
+	grpcMux := runtime.NewServeMux(jsonOption)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err = pb.RegisterSimpleBankHandlerServer(ctx, grpcMux, server)
+	if err != nil {
+		log.Fatal("cannot register handler server")
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+
+	listener, err := net.Listen("tcp", config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal("cannot creat grpc listener")
+	}
+	log.Printf("start http gateway server at %s", listener.Addr().String())
+	err = http.Serve(listener, mux)
+	if err != nil {
+		log.Fatal("cannot create HTTP Gateway server")
+	}
+}
+
 func runGinServer(config util.Config, store *db.Store) {
 
 	server, err := api.NewServer(config, store)
