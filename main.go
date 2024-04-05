88c329a88e1f18e2da003d11f9a2102dca1a2504
vexsx <p.gheiratian1382@yahoo.com>
2024-04-05 15:30:55 +0200

    JUST HTTP

--- main.go
+++ main.go
@@ -5,13 +5,10 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/vexsx/Simple-Bank/api"
 	db "github.com/vexsx/Simple-Bank/db/sqlc"
-	"github.com/vexsx/Simple-Bank/gapi"
-	"github.com/vexsx/Simple-Bank/pb"
+
 	"github.com/vexsx/Simple-Bank/util"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
+
 	"log"
-	"net"
 )
 
 func main() {
@@ -27,30 +24,11 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
-	runGrpcServer(config, store)
+
+	runGinServer(config, store)
 
 }
 
-func runGrpcServer(config util.Config, store *db.Store) {
-	server, err := gapi.NewServer(config, store)
-	if err != nil {
-		log.Fatal("cannot create server", err)
-	}
-
-	grpcServer := grpc.NewServer()
-	pb.RegisterSimpleBankServer(grpcServer, server)
-	reflection.Register(grpcServer)
-
-	listener, err := net.Listen("tcp", config.GRPCServerAddress)
-	if err != nil {
-		log.Fatal("cannot creat grpc listener")
-	}
-	log.Printf("start grpc server at %s", listener.Addr().String())
-	err = grpcServer.Serve(listener)
-	if err != nil {
-		log.Fatal("cannot creat grpc server")
-	}
-}
 func runGinServer(config util.Config, store *db.Store) {
 
 	server, err := api.NewServer(config, store)
