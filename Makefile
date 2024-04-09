3006d47d7622b0d04185a15df339edac47afdf91
vexsx <p.gheiratian1382@yahoo.com>
2024-04-09 20:24:05 +0200

    add grpc http gateway

--- Makefile
+++ Makefile
@@ -28,6 +28,7 @@ db_schema:
 proto:
 	protoc --proto_path=proto --go_out=pb --go_opt=paths=source_relative \
         --go-grpc_out=pb --go-grpc_opt=paths=source_relative \
+        --grpc-gateway_out=pb  --grpc-gateway_opt=paths=source_relative \
         proto/*.proto
 
 evans:
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"github.com/vexsx/Simple-Bank/api"
 	db "github.com/vexsx/Simple-Bank/db/sqlc"
@@ -10,6 +12,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"net"
+	"net/http"
 
 	"github.com/vexsx/Simple-Bank/util"
 
@@ -30,6 +33,7 @@ func main() {
 
 	store := db.NewStore(conn)
 
+	go runGatewayServer(config, store)
 	runGrpcServer(config, store)
 
 }
@@ -67,5 +71,34 @@ func runGrpcServer(config util.Config, store *db.Store) {
 	err = grpcServer.Serve(listener)
 	if err != nil {
 		log.Fatal("cannot start Grpc server", err)
+	}
+}
+
+func runGatewayServer(config util.Config, store *db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server", err)
+	}
+
+	grpcMux := runtime.NewServeMux()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	err = pb.RegisterSimpleBankHandlerServer(ctx, grpcMux, server)
+	if err != nil {
+		log.Fatal("cannot create grpc gateway", err)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+	listener, err := net.Listen("tcp", config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener", err)
+	}
+
+	log.Printf("start HTTP gateway server at %s", listener.Addr().String())
+
+	err = http.Serve(listener, mux)
+	if err != nil {
+		log.Fatal("cannot start HTTP gateway  server", err)
 	}
 }
