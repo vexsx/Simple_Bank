467f228d6ff56fc7f602ee578370c0251e2317c2
vexsx <p.gheiratian1382@yahoo.com>
2024-04-15 16:40:49 +0200

    email done

---
+++ mail/sender_test.go
@@ -0,0 +1,29 @@
+package mail
+
+import (
+	"github.com/stretchr/testify/require"
+	"github.com/vexsx/Simple-Bank/util"
+	"testing"
+)
+
+func TestSendEmailWithGmail(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	config, err := util.LoadConfig("..")
+	require.NoError(t, err)
+
+	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+
+	subject := "A test email"
+	content := `
+	<h1>Hello world</h1>
+	<p>This is a test message from <a href="http://techschool.guru">vexsx School</a></p>
+	`
+	to := []string{"p.gheiratian1382@yahoo.com"}
+	attachFiles := []string{"../README.md"}
+
+	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+	require.NoError(t, err)
+}
